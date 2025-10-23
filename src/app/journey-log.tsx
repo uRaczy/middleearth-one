a95e1d47f6a664076ce17ed5e712d3a75ac303a5
uRaczy <raczy7@gmail.com>
2025-10-23 22:39:24 +0200

    add dependencies

---
+++ src/app/journey-log.tsx
@@ -0,0 +1,13 @@
+const JourneyLog = () => {
+  <article>
+    <h2>23-10-25</h2>
+    <p>
+      {`Came back to this project after few months since initial idea
+      Up until now i found a suitable svg map of the Middle Earth and decided for a stack
+      I'll add some dependencies and decided to create this log
+      Next time I'll add some support for markdown to make notes for this process easier`}
+    </p>
+  </article>;
+};
+
+export default JourneyLog;
